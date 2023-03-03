194919257878d26e6eb9ccaaa0cb06977885040b
umarraza1423 <umarraza1423@gmail.com>
2023-03-03 11:35:52 +0500

    my first programme

--- hello world.py
+++ hello world.py
@@ -1,1 +1,11 @@
-print("hello i am Muhammad umar & i am also a python developer")
+print("\t""my profile")
+name="umar"
+print("name:", name)
+last_name="raza"
+print("last_name:", last_name)
+programe="BS_RS&GISC"
+print("programe:", programe)
+age="birth=16/10/2001"
+print("age", age)
+semester="4th"
+print("semester:", semester)
