0cda29de6c70aafa3ad20da846b4ef4f88c6ff15
umarraza1423 <umarraza1423@gmail.com>
2023-03-03 11:46:28 +0500

    my first commit

--- hello world.py
+++ hello world.py
@@ -1,11 +1,1 @@
-print("\t""my profile")
-name="umar"
-print("name:", name)
-last_name="raza"
-print("last_name:", last_name)
-programe="BS_RS&GISC"
-print("programe:", programe)
-age="birth=16/10/2001"
-print("age", age)
-semester="4th"
-print("semester:", semester)
+print("i am umar_raza & i am student in ist karachi also a python developer")
